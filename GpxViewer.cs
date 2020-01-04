d67a8b711606414402c9d1114b551bac2129bfcc
Li, Xinsheng <lixshnew@gmail.com>
2020-01-04 19:02:36 +0800

    v1.0
    Signed-off-by: Li, Xinsheng <lixshnew@gmail.com>

---
+++ GpxViewer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Windows.Forms;
+
+using OY.TotalCommander.TcPluginInterface;
+using OY.TotalCommander.TcPluginInterface.Lister;
+using System.Diagnostics;
+
+
+namespace OY.TotalCommander.TcPlugins.GpxViewer
+{
+    public class GpxViewer : ListerPlugin
+    {
+
+        public ListerControl lc;
+        public GpxViewer(StringDictionary pluginSettings) : base(pluginSettings)
+        {
+            if (String.IsNullOrEmpty(Title))
+                Title = ".NET GpxViewer";
+            DetectString = "EXT=\"GPX\"";
+        }
+        
+        public override object Load(string fileToLoad, ShowFlags showFlags)
+        {
+            lc = null;
+            if (!String.IsNullOrEmpty(fileToLoad))
+            {
+                lc = new ListerControl(fileToLoad);
+                FocusedControl = lc.webBrowser;
+            }
+            
+            return lc;
+        }
+        /*
+        public override ListerResult LoadNext(object control, string fileToLoad, ShowFlags showFlags)
+        {
+            return ListerResult.OK;
+        }
+
+        public override void CloseWindow(object control)
+        {
+
+        }
+
+        public override ListerResult SearchText(object control, string searchString, SearchParameter searchParameter)
+        {
+            return ListerResult.OK;
+        }
+
+        public override ListerResult SendCommand(object control, ListerCommand command, ShowFlags parameter)
+        {
+            return ListerResult.OK;
+        }
+
+        public override ListerResult Print(object control, string fileToPrint, string defPrinter,
+                PrintFlags printFlags, PrintMargins pMargins)
+        {
+            return ListerResult.OK;
+        }
+
+        public override int NotificationReceived(object control, int message, int wParam, int lParam)
+        {
+            // do nothing
+            return 0;
+        }
+
+        public override Bitmap GetPreviewBitmap(string fileToLoad, int width, int height, byte[] contentBuf)
+        {
+            Bitmap bitmap = new Bitmap(width, height);
+            return bitmap;
+        }
+
+        public override ListerResult SearchDialog(object control, bool findNext)
+        {
+            return ListerResult.OK;
+        }
+
+
+
+        public override void OnTcTrace(TraceLevel level, string text)
+        {
+
+        }
+        */
+    }
+}
---
+++ UserControl1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace OY.TotalCommander.TcPlugins.GpxViewer
+{
+    partial class ListerControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScrollBarsEnabled = false;
+            this.webBrowser.Size = new System.Drawing.Size(800, 450);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.Url = new System.Uri("", System.UriKind.Relative);
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // ListerControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.webBrowser);
+            this.Name = "ListerControl";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.Load += new System.EventHandler(this.ListerControl_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
